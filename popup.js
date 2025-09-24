dbe4c7c10a984ec727aa469a315d5d10da0a2778
stephenjoshi <stephenjoshi17@gmail.com>
2025-09-24 16:12:32 -0500

    added user facing popup site

---
+++ popup.js
@@ -0,0 +1,7 @@
+/**
+ * popup.js
+ *
+ * This script is reserved for handling interactions within popup.html.
+ * For the initial version, no interactive elements are present,
+ * so this file is intentionally left empty.
+ */
